08306b82b74f0d661a85a354948697efa35ae596
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-05-15 15:12:06 -0300

    feat: controller to get single customer

--- src/controllers/CustomersController.js
+++ src/controllers/CustomersController.js
@@ -9,3 +9,15 @@ export async function getCustomers(req, res) {
     }
 }
 
+export async function getCustomerById(req, res) {
+    const { id } = req.params
+    try {
+        const customer = await db.query("SELECT * FROM customers WHERE id = $1;", [id])
+        if (!customer.rows[0]) return res.sendStatus(404)
+        res.send(customer.rows[0])
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
