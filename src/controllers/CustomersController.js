ab4cd82aa235f3bdef20dca83c5547cd84ec0684
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-05-15 15:13:52 -0300

    feat: controller to insert new customer into database

--- src/controllers/CustomersController.js
+++ src/controllers/CustomersController.js
@@ -2,22 +2,36 @@ import db from "../config/database.js";
 
 export async function getCustomers(req, res) {
     try {
-        const customers = await db.query("SELECT * FROM customers;")
-        res.send(customers.rows)
+        const customers = await db.query("SELECT * FROM customers;");
+        
+        res.send(customers.rows);
     } catch (error) {
-        res.status(500).send(error.message)
+        res.status(500).send(error.message);
     }
 }
 
 export async function getCustomerById(req, res) {
     const { id } = req.params
     try {
-        const customer = await db.query("SELECT * FROM customers WHERE id = $1;", [id])
-        if (!customer.rows[0]) return res.sendStatus(404)
-        res.send(customer.rows[0])
+        const customer = await db.query("SELECT * FROM customers WHERE id = $1;", [id]);
+        if (!customer.rows[0]) return res.sendStatus(404);
 
+        res.send(customer.rows[0]);
     } catch (error) {
-        res.status(500).send(error.message)
+        res.status(500).send(error.message);
+    }
+}
+
+export async function createCustomer(req, res) {
+    const { name, phone, cpf, birthday } = req.body;
+
+    try {
+        await db.query("INSERT INTO customers (name,phone,cpf,birthday) VALUES ($1,$2,$3,$4);",
+            [name, phone, cpf, birthday]);
+
+        res.sendStatus(201);
+    } catch (error) {
+        res.status(500).send(error.message);
     }
 }
 
