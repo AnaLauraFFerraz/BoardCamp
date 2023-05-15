fbfb2a6edc32cbd52923ad536a0267c306c1ffc0
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-05-15 15:34:31 -0300

    feat: games router

---
+++ src/routers/GamesRouter.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createGame, getGames } from "../controllers/GamesController.js";
+import { gameValidation } from "../middlewares/GameMiddleware.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import { gameSchema } from "../schemas/GameSchema.js";
+
+const gamesRouter = Router()
+
+gamesRouter.get("/games",getGames)
+gamesRouter.post("/games",validateSchema(gameSchema),gameValidation,createGame)
+
+export default gamesRouter
