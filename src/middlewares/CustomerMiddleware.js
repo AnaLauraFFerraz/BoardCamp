7023acf0686de902f46f552a26a60dd486fb779a
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-05-15 15:03:07 -0300

    feat: validation to insert game

--- src/middlewares/CustomerMiddleware.js
+++ src/middlewares/CustomerMiddleware.js
@@ -1,24 +1,28 @@
 import db from "../config/database.js";
 
 export async function customerValidation(req, res, next) {
-    const {cpf} = req.body
+    const {cpf} = req.body;
+
     try {
         const checkCustomer = await db.query("SELECT * FROM customers WHERE cpf = $1",[cpf]);
-        if (checkCustomer.rows[0]) return res.sendStatus(409)
+        if (checkCustomer.rows[0]) return res.sendStatus(409);
     } catch (error) {
-        return res.status(500).send(error.message)
+        return res.status(500).send(error.message);
     }
+    
     next();
 }
 
 export async function customerIdValidation(req, res, next) {
-    const {id} = req.params
-    const {cpf} = req.body
+    const {id} = req.params;
+    const {cpf} = req.body;
+
     try {
         const checkCustomer = await db.query("SELECT * FROM customers WHERE cpf = $1",[cpf]);
-        if (checkCustomer.rows[0] && checkCustomer.rows[0].id !== Number(id) ) return res.sendStatus(409)
+        if (checkCustomer.rows[0] && checkCustomer.rows[0].id !== Number(id) ) return res.sendStatus(409);
     } catch (error) {
-        return res.status(500).send(error.message)
+        return res.status(500).send(error.message);
     }
+
     next();
 }
---
+++ src/middlewares/GameMiddleware.js
@@ -0,0 +1,14 @@
+import db from "../config/database.js";
+
+export async function gameValidation(req, res, next) {
+    const {name} = req.body;
+
+    try {
+        const checkGame = await db.query("SELECT * FROM games WHERE name = $1",[name]);
+        if (checkGame.rows[0]) return res.sendStatus(409);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+
+    next();
+}
