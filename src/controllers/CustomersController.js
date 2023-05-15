d0f259e16c407f607d7d4bb9db5e273a9f0a4a85
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-05-15 15:11:08 -0300

    feat: controller to get customers list

---
+++ src/controllers/CustomersController.js
@@ -0,0 +1,11 @@
+import db from "../config/database.js";
+
+export async function getCustomers(req, res) {
+    try {
+        const customers = await db.query("SELECT * FROM customers;")
+        res.send(customers.rows)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
