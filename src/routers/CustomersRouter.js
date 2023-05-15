80f4b56bf7568e9d6ca525286b898262f419fe49
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-05-15 15:34:15 -0300

    feat: customer router

---
+++ src/routers/CustomersRouter.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { createCustomer, getCustomerById, getCustomers, updateCustomer } from "../controllers/CustomersController.js";
+import { customerValidation, customerIdValidation } from "../middlewares/CustomerMiddleware.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import { customerSchema } from "../schemas/CustomerSchema.js";
+
+const customersRouter = Router()
+
+customersRouter.get("/customers",getCustomers)
+customersRouter.get("/customers/:id",getCustomerById)
+customersRouter.post("/customers",validateSchema(customerSchema),customerValidation,createCustomer)
+customersRouter.put("/customers/:id",validateSchema(customerSchema),customerIdValidation,updateCustomer)
+
+export default customersRouter
