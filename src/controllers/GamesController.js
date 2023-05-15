875fc16d27632a722766ae9527658c0bae5dc737
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-05-15 15:16:20 -0300

    feat: controller to insert new game into database

--- src/controllers/GamesController.js
+++ src/controllers/GamesController.js
@@ -10,3 +10,15 @@ export async function getGames(req,res) {
     }
 }
 
+export async function createGame (req,res) {
+    const {name,image,stockTotal,pricePerDay} = req.body;
+
+    try {
+        await db.query('INSERT INTO games (name,image,"stockTotal","pricePerDay") values ($1,$2,$3,$4);',
+            [name,image,stockTotal,pricePerDay]);
+
+        res.sendStatus(201);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
